e3e1fdb936703ff3372486cc32221f2c00094ff7
JeO <jeo2k1@gmail.com>
2023-06-14 13:13:01 -0300

    Commit Inicial

---
+++ EMBARGOS/embagos_sarha-oracle.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import cx_Oracle
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
+import openpyxl
+import subprocess
+import shutil
+import os
+
+# Ingresar numero de liquidacion
+numero_liquidacion = int(input('Ingrese el numero de liquidacion: '))
+
+# CONECTA CON LA VPN DE SARHA
+subprocess.call([r"EMBARGOS\CONECTA_VPN.BAT"])
+
+try:
+   # CONECTA CON LA BBDD ORACLE DE SARHA
+   engine = sqlalchemy.create_engine("oracle+cx_oracle://jorellana:R3L4N43@10.2.2.21:1521/SAXE2012")
+   # EJECUTA LA QUERY PARA OBTENER LOS EMBARGOS JUDICIALES
+   embargos_sql = f"""SELECT cl.nro_liquidacion, EL.CUIT, CO.DESCRIPCION as ORGANISMO, EL.CUIL, EL.APELLIDO, EL.NOMBRE,  CL.COD_CONCEPTO, CL.COD_SUBCONCEPTO, CP.DESCRIPCION AS DESCRIPCION_CAUSA, O.CAUSA_JUDICIAL, cl.valor
+FROM SARHA.concepto_liquidacion CL,
+    SARHA.empleado_liquidacion EL,
+    SARHA.cuit_organismo CO,
+    SARHA.embargo_concepto EC,
+    SARHA.OFICIO O,
+    SARHA.CONCEPTO CP
+
+WHERE CL.CUIL = EL.CUIL 
+    AND CL.NRO_LIQUIDACION = EL.NRO_LIQUIDACION 
+    AND EL.CUIT = CO.CUIT
+    AND EC.CUIL = CL.CUIL
+    AND CL.COD_CONCEPTO = EC.COD_CONCEPTO
+    AND CL.COD_CONCEPTO = CP.COD_CONCEPTO
+    AND CL.COD_SUBCONCEPTO = EC.COD_SUBCONCEPTO
+    AND EC.COD_OFICIOS = O.COD_OFICIOS
+    AND CL.nro_liquidacion = {numero_liquidacion}
+    AND (CL.COD_CONCEPTO = 481 OR CL.COD_CONCEPTO = 482) 
+    AND CL.VALOR < 0
+""";
+   # CREA EL DATAFRAME DE EMBARGOS DE LA CONSULTA SQL
+   df_embargos = pd.read_sql(embargos_sql, engine)
+   
+   # MUESTRA TODOS LOS ORGANISMOS QUE EXISTEN EN LOS EMBARGOS
+   print(df_embargos['organismo'].unique())
+   
+   # SEPARA LOS EMBARGOS Y GENERA LOS EXCEL DE CADA ORGANISMO
+   cap_df = df_embargos[df_embargos['organismo'] == '(CAP) CONSEJO AGRARIO PROVINCIAL']
+   cap_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-CAPR.xlsx', index=False)   
+   
+   mds_df = df_embargos[df_embargos['organismo'] == '(MDS) MINISTERIO DE DESARROLLO SOCIAL']
+   mds_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-DESA.xlsx', index=False)
+   
+   mpci_df = df_embargos[df_embargos['organismo'] == '(MPCI) MINISTERIO DE PRODUCCIÓN COMERCIO E INDUSTRIA']
+   mpci_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-PROD.xlsx', index=False)
+   
+   mtes_df = df_embargos[df_embargos['organismo'] == '(MTES) MINISTERIO TRABAJO, EMPLEO Y SEG. SOCIAL']
+   mtes_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-TRAB.xlsx', index=False)
+   
+   mgo_df = df_embargos[df_embargos['organismo'] == '(MGO) MINISTERIO DE GOBIERNO']
+   mgo_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-MGOB.xlsx', index=False)
+   
+   mefi_df = df_embargos[df_embargos['organismo'] == '(MEFI) MINISTERIO DE ECONOMIA, FINANZAS E INFRAESTRUCTURA']
+   mefi_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-MEFI.xlsx', index=False)
+   
+   msgg_df = df_embargos[df_embargos['organismo'] == '(MSGG) MINISTERIO SECRETARIA GENERAL DE LA GOBERNACION']
+   msgg_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-MSGG.xlsx', index=False)
+   
+   mseg_df = df_embargos[df_embargos['organismo'] == '(MSEG) MINISTERIO DE SEGURIDAD']
+   mseg_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-SEGU.xlsx', index=False)
+   
+   csc_df = df_embargos[df_embargos['organismo'] == '(CSC) CASA DE SANTA CRUZ']
+   csc_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-CASA.xlsx', index=False)
+   
+   gob_df = df_embargos[df_embargos['organismo'] == '(GOB) GOBERNACIÓN']
+   gob_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-GOBE.xlsx', index=False)
+   
+   jgm_df = df_embargos[df_embargos['organismo'] == '(JGM) MINISTERIO JEFATURA DE GABINETE DE MINISTROS']
+   jgm_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-JGAB.xlsx', index=False)
+   
+   htd_df = df_embargos[df_embargos['organismo'] == '(HTD) HONORABLE TRIBUNAL DISCIPLINARIO']
+   htd_df.to_excel('EMBARGOS\SALIDA/EMBARGOS-HTDI.xlsx', index=False)
+   
+   # TERMINA LA CONEXION DE LA VPN
+   subprocess.call([r"EMBARGOS\DESCONECTA_VPN.BAT"])
+   # COPIA ARCHIVOS EXCEL A CARPETA EMBARGOS
+   ruta_origen="EMBARGOS\SALIDA"
+   ruta_destino="S:/LDDAT/SARHA/EMBARGOS"
+   shutil.copytree(ruta_origen, ruta_destino, dirs_exist_ok=True)
+except SQLAlchemyError as e:
+   print(e)
---
+++ LIQ-VERTICAL/liq_vertical_sarha.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import cx_Oracle
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
+import openpyxl
+import subprocess
+import shutil
+import os
+
+# Ingresar numero de liquidacion
+numero_liquidacion = int(input('Ingrese el numero de liquidacion: '))
+
+# CONECTA CON LA VPN DE SARHA
+subprocess.call([r"LIQ-VERTICAL\CONECTA_VPN.BAT"])
+
+try:
+   # CONECTA CON LA BBDD ORACLE DE SARHA
+   engine = sqlalchemy.create_engine("oracle+cx_oracle://jorellana:R3L4N43@10.2.2.21:1521/SAXE2012")
+   # EJECUTA LA QUERY PARA OBTENER LOS EMBARGOS JUDICIALES
+   embargos_sql = f"""SELECT 
+	el.nro_liquidacion,
+    el.cuit,
+    c.descripcion,
+    cl.periodo_desde,
+    cl.cuil,
+    el.apellido,
+    el.nombre,
+    el.cod_escalafon,
+    el.descripcion_escalafon,
+    el.cod_funcion,
+    el.descripcion_funcion,
+    cl.cod_concepto,
+    cl.descripcion_concepto,
+    cl.cod_subconcepto,
+    cl.descripcion_subconcepto,
+    cl.valor
+    
+from 
+    sarha.concepto_liquidacion cl,
+    sarha.empleado_liquidacion el,
+    sarha.cuit_organismo c
+
+where 
+    cl.nro_liquidacion = {numero_liquidacion} 
+    and el.cuit = c.cuit
+    and el.cuil = cl.cuil
+    and el.nro_liquidacion = cl.nro_liquidacion
+order by
+    cl.cuil
+""";
+   # CREA EL DATAFRAME DE EMBARGOS DE LA CONSULTA SQL
+   df_vertical = pd.read_sql(embargos_sql, engine)
+       
+   df_vertical.to_excel(f'LIQ-VERTICAL\SALIDA\LIQ-VERTICAL-{numero_liquidacion}.xlsx', index=False)
+   
+   # TERMINA LA CONEXION DE LA VPN
+   subprocess.call([r"LIQ-VERTICAL\DESCONECTA_VPN.BAT"])
+   # COPIA ARCHIVOS EXCEL A CARPETA EMBARGOS
+   ruta_origen="LIQ-VERTICAL\SALIDA"
+   ruta_destino="S:/LDDAT/SARHA/"
+   shutil.copytree(ruta_origen, ruta_destino, dirs_exist_ok=True)
+except SQLAlchemyError as e:
+   print(e)
