e421a263c93bddd3ac9b018fc361c7e44ebda5a8
JeO <jeo2k1@gmail.com>
2023-06-14 14:41:15 -0300

    add la caja de ahorro y seguro

---
+++ LA-CAJA-AHORRO/css_aportes_sarha.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import cx_Oracle
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
+import openpyxl
+import subprocess
+import shutil
+import os
+
+# Ingresar numero de liquidacion
+numero_liquidacion = int(input('Ingrese el numero de liquidacion: '))
+
+# CONECTA CON LA VPN DE SARHA
+subprocess.call([r"LA-CAJA-AHORRO\CONECTA_VPN.BAT"])
+
+try:
+   # CONECTA CON LA BBDD ORACLE DE SARHA
+   engine = sqlalchemy.create_engine("oracle+cx_oracle://jorellana:R3L4N43@10.2.2.21:1521/SAXE2012")
+   # EJECUTA LA QUERY PARA OBTENER LOS DESCUENTOS DEL 4% y 6,4%
+   embargos_sql = f"""SELECT * 
+FROM (
+    SELECT 
+        el.cuit,
+        co.descripcion,
+        cl.cuil,
+        el.apellido, 
+        el.nombre,
+        em.fecha_nacimiento,
+        sum(case when cl.cod_clase_concepto = 1 and not cl.cod_concepto = 70 and not cl.cod_concepto = 617 then  cl.valor else 0 end) as REMUNERATIVO,
+        sum(case when cl.cod_concepto = 352 then cl.valor else 0 end) as CODIGO352,
+        sum(case when cl.cod_concepto = 353 then cl.valor else 0 end) as CODIGO353,
+        sum(case when cl.cod_concepto = 354 then cl.valor else 0 end) as CODIGO354,
+        sum(case when cl.cod_concepto = 355 then cl.valor else 0 end) as CODIGO355,
+        sum(case when cl.cod_concepto = 356 then cl.valor else 0 end) as CODIGO356
+    FROM 
+        sarha.concepto_liquidacion cl,
+        sarha.empleado_liquidacion el,
+        sarha.cuit_organismo co,
+        sarha.empleado em
+    where 
+        el.nro_liquidacion = cl.nro_liquidacion
+        and em.cuil = el.cuil
+        and el.cuil = cl.cuil
+        and el.cuit = co.cuit
+        and cl.nro_liquidacion = {numero_liquidacion}
+        and el.no_paga is null
+    
+     group by el.cuit, co.descripcion, cl.cuil, el.apellido, el.nombre, em.fecha_nacimiento
+     )
+WHERE 
+    not (CODIGO352 = 0 and CODIGO353 = 0 and CODIGO354 = 0 and CODIGO355 = 0 and CODIGO356 = 0)
+""";
+   # CREA EL DATAFRAME DE EMBARGOS DE LA CONSULTA SQL
+   df_vertical = pd.read_sql(embargos_sql, engine)
+       
+   df_vertical.to_excel(f'LA-CAJA-AHORRO\SALIDA\LA CAJA AHORRO SEGUROS.xlsx', index=False)
+   
+   # TERMINA LA CONEXION DE LA VPN
+   subprocess.call([r"LA-CAJA-AHORRO\DESCONECTA_VPN.BAT"])
+   # COPIA ARCHIVOS EXCEL A CARPETA EMBARGOS
+   ruta_origen="LA-CAJA-AHORRO\SALIDA"
+   ruta_destino="S:/LDDAT/SARHA/DESCUENTOS"
+   shutil.copytree(ruta_origen, ruta_destino, dirs_exist_ok=True)
+except SQLAlchemyError as e:
+   print(e)
