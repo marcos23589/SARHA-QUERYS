982d3563ab3503fbbf1df1ae27494bb6d52a442d
Marcos Daniel Olivera <marcos.olivera@mefi.gob.ar>
2024-09-05 13:12:36 -0300

    [add+fix] carga de archivo txt, rename, refactoreo

--- GANANCIAS/liq_formateo.py
+++ GANANCIAS/liq_formateo.py
@@ -1,15 +1,40 @@
 import datetime
 import pandas as pd
 from tkinter import filedialog
+from pathlib import Path
 
-# se carga el archivo TXT que sale desde el programa LIQAFIP.COB
+# si se carga el archivo TXT que sale desde el programa LIQAFIP.COB, es para crear el excel
+# si se carga el archivo XLS, es para eliminar los CUILES repetidos y sumar los importes
+
 
 # cuadro de carga del archivo
 def cargar_archivo():
-    archivo = filedialog.askopenfilename(filetypes=[("Archivos TXT", "*.txt")])
-    print(type(archivo))
-    df = pd.read_csv(archivo, sep=";", skipinitialspace=True, encoding="latin-1")
+    archivo = filedialog.askopenfilename(
+        filetypes=[
+            ("Archivos XLSX", "*.xlsx"),
+            ("Archivos XLS", "*.xls"),
+            ("Archivos TXT", "*.txt"),
+        ]
+    )
+
+    # obtenemos la extension del archivo cargado
+    extension = Path(archivo).suffix
+
+    if extension == ".txt" or extension == ".TXT":
+        df = pd.read_csv(archivo, sep=";", skipinitialspace=True, encoding="latin-1")
+        # se elimina la ultima columna vacia
+        last_column = df.columns[-1]
+        df = df.drop(last_column, axis=1)
+
+        # se agregan las cabeceras de las columnas
+        df.columns = columnas
+        
+        # Divide los valores por 100
+        df[columnas_a_sumar] = df[columnas_a_sumar] / 100
+    else:
+        df = pd.read_excel(archivo, sheet_name="Hoja1")
     return df
+
 
 # Lista personalizada para los nombres de las columnas
 columnas = [
@@ -29,31 +54,14 @@ columnas = [
     "SEGURO DE VIDA",
 ]
 
-# se elimina la ultima columna vacia
-df = cargar_archivo()
-last_column = df.columns[-1]
-df = df.drop(last_column, axis=1)
+# Definir las columnas que deben sumarse
+columnas_a_sumar = columnas[5:]
 
-# se agregan las cabeceras de las columnas
-df.columns = columnas
-
-# se ordena el DataFrame por CUIL, y luego por REMUNERACIONES
-ordenado = df.sort_values(by=["CUIL", "REMUNERACIONES"], ascending=[True, False])
-
-
+# funcion que ordena el DF por CUIL y suma los valores
 def consolidar_cuiles(df):
-    # Definir las columnas que deben sumarse
-    columnas_a_sumar = [
-        "REMUNERACIONES",
-        "ASIGNACIONES FLIARES",
-        "HS EXTRAS",
-        "SAC",
-        "%REMU",
-        "APORTES JUBILATORIOS",
-        "APORTES O.SOCIAL",
-        "SINDICATO",
-        "SEGURO DE VIDA",
-    ]
+
+    # se agregan las cabeceras de las columnas
+    df.columns = columnas
 
     # Identificar las filas con CUIL duplicados
     cuil_duplicados = df[df.duplicated("CUIL", keep=False)]
@@ -80,19 +88,21 @@ def consolidar_cuiles(df):
 
     # Agregar las filas consolidadas al DataFrame original
     df = pd.concat([df, df_consolidado], ignore_index=True)
-
-    # Dividir los valores por 100
-    df[columnas_a_sumar] = df[columnas_a_sumar] / 100
-
+    
     return df
 
+df = cargar_archivo()
 
-# se le aplica la funcion al DataFrame ordenado
+# se ordena el DataFrame por CUIL, y luego por REMUNERACIONES
+ordenado = df.sort_values(by=["CUIL", "REMUNERACIONES"], ascending=[True, False])
+
+# se le aplica la funcion de suma de valores y eliminacion de cuiles repetidos al DataFrame ordenado
 df_consolidado = consolidar_cuiles(ordenado)
 
 # se convierte el DataFrame a Excel
+nombre = datetime.datetime.now().microsecond
 df_consolidado.to_excel(
-    f"SALIDA/salida-{datetime.datetime.now().microsecond}.xlsx",
+    f"SALIDA/liq-{nombre}.xlsx",
     sheet_name="hoja1",
     header=True,
     index=False,
